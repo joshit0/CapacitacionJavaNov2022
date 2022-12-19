7e8b2bb896ed0012e4b6fc50747235ec6508f19f
Lisset Torres <lissetjensen1984@gmail.com>
2022-12-18 18:34:54 -0600

    Agregar Metodo

--- src/test/java/automation/lisset/ejercicio.java
+++ src/test/java/automation/lisset/ejercicio.java
@@ -1,4 +1,20 @@
 package automation.lisset;
 
+import org.testng.annotations.Test;
+
 public class ejercicio {
+
+    @Test
+    public void realizarOperacion2() {
+        int numero1 = 10;
+        int numero2 = 20;
+
+        suma(numero1,numero2);
+    }
+
+    public void suma(int a, int b){
+        int suma;
+        suma=a+b;
+        System.out.println("La suma es "+ suma);
+    }
 }
